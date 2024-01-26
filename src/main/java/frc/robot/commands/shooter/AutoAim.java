10054a18d78f53bf0ddc8014de68ad8b1845bc33
cherriae <zeruiz@nycstudents.net>
2024-01-26 13:55:30 -0500

    autoaiming shooter

--- src/main/java/frc/robot/commands/shooter/AutoAim.java
+++ src/main/java/frc/robot/commands/shooter/AutoAim.java
@@ -3,8 +3,14 @@
 
 package frc.robot.commands.shooter;
 
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
@@ -14,13 +20,34 @@ public class AutoAim extends Command {
   private VisionSubsystem _vision;
   private SwerveDriveSubsystem _swerve;
 
+  private DoubleSupplier _xSpeed;
+  private DoubleSupplier _ySpeed;
+
+  private PIDController _headingController = new PIDController(
+    Constants.PID.PP_ROTATION.kP,
+    0,
+    0
+  );
+
   /** Creates a new AutoAim. */
-  public AutoAim(ShooterSubsystem shooter, VisionSubsystem vision, SwerveDriveSubsystem swerve) {
+  public AutoAim(
+    ShooterSubsystem shooter, 
+    VisionSubsystem vision, 
+    SwerveDriveSubsystem swerve,
+    DoubleSupplier xSpeed,
+    DoubleSupplier ySpeed
+  ) {
     // Use addRequirements() here to declare subsystem dependencies.
     _shooter = shooter;
     _vision = vision;
     _swerve = swerve;
-    addRequirements(_shooter, _vision); // not adding swerve as a requirement to prevent interrupting default command
+
+    _xSpeed = xSpeed;
+    _ySpeed = ySpeed;
+
+    _headingController.setTolerance(2);
+
+    addRequirements(_shooter, _vision, _swerve);
   }
 
   // Called when the command is initially scheduled.
@@ -30,17 +57,31 @@ public class AutoAim extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double shooterAngle = 0;
+    double desiredShooterAngle = 0;
+    double desiredSwerveHeading = 0;
+
+    double currentSwerveHeading = _swerve.getHeading().getDegrees();
 
     if (_vision.isApriltagVisible()) {
-      shooterAngle = _vision.shooterAnglesToSpeaker()[1]; // ty
+      desiredShooterAngle = _vision.anglesToSpeaker()[1]; // ty
+      desiredSwerveHeading = _vision.anglesToSpeaker()[0];
+
     } else {
-      shooterAngle = _swerve.shooterAngleToSpeaker();
+      desiredShooterAngle = _swerve.anglesToSpeaker()[1];
+      desiredSwerveHeading = _swerve.anglesToSpeaker()[0];
     }
 
-    SmartDashboard.putNumber("ANGLE", shooterAngle);
+    desiredSwerveHeading += currentSwerveHeading;
 
-    _shooter.setAngle(shooterAngle);
+    _shooter.setAngle(desiredShooterAngle);
+
+    _swerve.driveChassis(
+      new ChassisSpeeds(
+        _xSpeed.getAsDouble() * Constants.Speeds.SWERVE_DRIVE_MAX_SPEED * Constants.Speeds.SWERVE_DRIVE_COEFF,
+        _ySpeed.getAsDouble() * Constants.Speeds.SWERVE_DRIVE_MAX_SPEED * Constants.Speeds.SWERVE_DRIVE_COEFF,
+        _headingController.calculate(currentSwerveHeading, desiredSwerveHeading)
+      )
+    );
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/utils/LimelightHelper.java
+++ src/main/java/frc/robot/utils/LimelightHelper.java
@@ -76,4 +76,6 @@ public class LimelightHelper {
 
     return null;
   }
+
+  
 }
