56d73f42e44699034d4706672747f8280a15c068
Jerry <zeruiz@nycstudents.net>
2024-04-07 13:38:49 -0400

    changed kP on note heading controller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -137,7 +137,7 @@ public final class Constants {
     public static final double ELEVATOR_KP = 5.2;
     public static final double ELEVATOR_KD = 0.2;
 
-    public static final double NOTE_HEADING_KP = 0.7;
+    public static final double NOTE_HEADING_KP = 0.07;
     public static final double NOTE_HEADING_KD = 0.0005;
 
     public static final double SWERVE_HEADING_KP = 0.15;
