17b49589fb73577fc7883ae6d03dc3ad432bee9b
Cherriae <zeruiz@nycstudents.net>
2024-10-29 13:53:01 -0400

    soft limit change on elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -36,7 +36,7 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     SoftwareLimitSwitchConfigs softLimits = new SoftwareLimitSwitchConfigs();
 
-    softLimits.ForwardSoftLimitThreshold = 0.5 * Physical.ELEVATOR_GEAR_RATIO / Physical.ELEVATOR_DISTANCE_PER_ROTATION;
+    softLimits.ForwardSoftLimitThreshold = 0.55 * Physical.ELEVATOR_GEAR_RATIO / Physical.ELEVATOR_DISTANCE_PER_ROTATION;
     softLimits.ReverseSoftLimitThreshold = 0;
 
     softLimits.ForwardSoftLimitEnable = true;
